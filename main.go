b0ad634f4acbcd43b5b972a8cb60795142647cf0
IllgenL <165781654+IllgenL@users.noreply.github.com>
2026-08-06 22:23:55 +0200

    Added custom endpoint

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/", handleReadiness)
 
 	server := &http.Server{
 		Handler: mux,
